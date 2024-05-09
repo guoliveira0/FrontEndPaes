4eb91ba916189fcffe83a5ae3616756114db3e06
Gustavo <luizgustavo160206@gmail.com>
2024-05-09 09:09:54 -0300

    arquivo padronizado

---
+++ resources/views/components/components/titles.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h2 class="h1">Olá meu nome é Luiz</h2>
+</div>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ola</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body id="body-background">
+<header>
+<x-navbar />
+</header>
+
+
+
+<footer>
+</footer>
+
+</body>
+
+</html>
